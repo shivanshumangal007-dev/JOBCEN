6c553bf7653067be4b45536e9a33762382d9f104
Hannu Verma <hannuverma78@gmail.com>
2026-07-24 15:11:18 +0530

    bug fixes

--- backend/app/api/routes/profile.py
+++ backend/app/api/routes/profile.py
@@ -1,8 +1,9 @@
+from pydantic import BaseModel
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.session import get_db
-from app.schemas.user import UserBase
+from app.schemas.user import UserBase, UserResponse
 from app.services.auth_services import get_current_user
 from app.services.auth_services import generate_and_send_otp
 from app.api.routes.auth import auth_limiter
@@ -10,17 +11,19 @@ from app.db.crud.user import get_user_by_id
 from app.db.crud.profile import get_current_user_profile
 router = APIRouter(prefix="/profile", tags=["user-profile"])
 
+class MeResponse(BaseModel):
+    profile: dict  # Or your UserProfileResponse schema
+    user: UserResponse
 
-
-@router.get("/me", dependencies=[Depends(auth_limiter)])
-async def me(current_user = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+@router.get("/me", response_model=MeResponse, dependencies=[Depends(auth_limiter)])
+async def me(current_user: UserResponse = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
     user_profile = await get_current_user_profile(current_user.id, db)
 
     if not user_profile:
         raise HTTPException(status_code=404, detail="User not found")
 
     
-    return {"profile":user_profile, "user":current_user}
+    return {"profile":user_profile, "user":UserResponse.model_validate(current_user)}
 
 @router.post("/delete-profile", dependencies=[Depends(auth_limiter)])
 async def delete_profile(current_user: UserBase = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
